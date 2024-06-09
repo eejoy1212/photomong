beb65a648c8de579bea6bb8a7169d9c719a8f93f
wonny <eejoy1212@gmail.com>
2024-06-09 12:50:02 +0900

    프로모션 코드 체크

--- app/src/screens/Payment/Promo.js
+++ app/src/screens/Payment/Promo.js
@@ -172,13 +172,23 @@ function Cash() {
 
      const checkReedeem = async () => {
           try {
-               const deviceNumber = process.env.REACT_APP_DEVICE_NUMBER;
-               const response = await fetch(`${process.env.REACT_APP_BE_PROD}/payments/api/redeem?device=${deviceNumber}&code=${redeemCode}&amount=${frameAmount}`);
-               const paymentData = await response.json();
-               if (paymentData.status === "OK") {
-                    sessionStorage.setItem('orderCodeNum', paymentData.order_code);
-                    navigate("/payment-result");
+               // const deviceNumber = process.env.REACT_APP_DEVICE_NUMBER;
+               // const response = await fetch(`${process.env.REACT_APP_BE_PROD}/payments/api/redeem?device=${deviceNumber}&code=${redeemCode}&amount=${frameAmount}`);
+              const response=await checkPromotionCode({
+               code:redeemCode
+              })
+               const paymentData = await response[0];
+               // console.log("체크 프로모션 코드",paymentData)
+               if (paymentData.message==="Promotion code is valid") {
+                    sessionStorage.setItem('orderCodeNum', redeemCode);
+                         navigate("/payment-result");
+               } else {
+                    
                }
+               // if (paymentData.status === "OK") {
+               //      sessionStorage.setItem('orderCodeNum', paymentData.order_code);
+               //      navigate("/payment-result");
+               // }
           } catch (error) {
                console.error(error);
           }
